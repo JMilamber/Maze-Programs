5c92662fa7fefc2aedd736b2bc1bc0aec87eb9ed
StringerJ <stringerj@allegheny.edu>
2020-02-19 14:56:42 -0500

    removed excess import statements and conformed code to 79 char limit in MazMainProgram.py

--- MazeMainProgram.py
+++ MazeMainProgram.py
@@ -2,29 +2,22 @@
 #       Date started - November 18th 2019
 
 import os
-import random
-import time
-import math
-import CheckValue
-import stringToNumber
 import tkinter
-from tkinter import constants
-from tkinter import ttk
-from tkinter import messagebox
 import subprocess
+
 
 def main():
     main = tkinter.Tk()
     working_Dir = os.getcwd()
-    directory_change = ""
+
     def MRBA():
-        directory_change = working_Dir + "\Modified_Recursive_Algorithm"
+        directory_change = working_Dir + "/Modified_Recursive_Algorithm"
         os.chdir(directory_change)
         subprocess.Popen(['Maze.py'], shell=True)
         os.chdir(working_Dir)
 
     def RDA():
-        directory_change = working_Dir + "\Recursive_Division"
+        directory_change = working_Dir + "/Recursive_Division"
         os.chdir(directory_change)
         subprocess.Popen(['RdMaze.py'], shell=True)
         os.chdir(working_Dir)
@@ -35,25 +28,40 @@ def main():
     def placeHolder2():
         pass
 
-    mainLabel = tkinter.Label(main, text="Algorithm Selector").grid(row=1, column=2, columnspan=2)
-    fillerMiddleTop = tkinter.Label(main, text="             ").grid(row=2, column=2, columnspan=2)
-    fillerMiddleBottom = tkinter.Label(main, text="             ").grid(row=6, column=2, columnspan=2)
-    fillerSide = tkinter.Label(main, text="             ").grid(row=7, column=7)
-    fillerSide1 = tkinter.Label(main, text="                ").grid(row=0, column=0)
-    ModifiedRecursiveAlgorithm = tkinter.Button(main, text="Modified Recursive Backtracker Algorithm", command=MRBA)
+    mainLabel = tkinter.Label(main,
+                              text="Algorithm Selector").grid(row=1,
+                                                              column=2,
+                                                              columnspan=2)
+    fillerMiddleTop = tkinter.Label(main,
+                                    text="             ").grid(row=2,
+                                                               column=2,
+                                                               columnspan=2)
+    fillerMiddleBottom = tkinter.Label(main,
+                                       text="             ").grid(row=6,
+                                                                  column=2,
+                                                                  columnspan=2)
+    fillerSide = tkinter.Label(main, text="             ").grid(row=7,
+                                                                column=7)
+    fillerSide1 = tkinter.Label(main, text="                ").grid(row=0,
+                                                                    column=0)
+    ModifiedRecursiveAlgorithm = tkinter.Button(main,
+                                                text="Modified Recursive"
+                                                + "Backtracker Algorithm",
+                                                command=MRBA)
     ModifiedRecursiveAlgorithm.grid(row=3, column=2, columnspan=2)
-    ModifiedRecursiveAlgorithm = tkinter.Button(main, text="Recursive Division Algorithm", command=RDA)
+    ModifiedRecursiveAlgorithm = tkinter.Button(main, text="Recursive Division"
+                                                + "Algorithm", command=RDA)
     ModifiedRecursiveAlgorithm.grid(row=4, column=2, columnspan=2)
-    ModifiedRecursiveAlgorithm = tkinter.Button(main, text="_____ Algorithm", command=placeHolder1)
+    ModifiedRecursiveAlgorithm = tkinter.Button(main, text="_____ Algorithm",
+                                                command=placeHolder1)
     ModifiedRecursiveAlgorithm.grid(row=5, column=2, columnspan=2)
-    ModifiedRecursiveAlgorithm = tkinter.Button(main, text="_____ Algorithm", command=placeHolder2)
+    ModifiedRecursiveAlgorithm = tkinter.Button(main, text="_____ Algorithm",
+                                                command=placeHolder2)
+
     ModifiedRecursiveAlgorithm.grid(row=6, column=2, columnspan=2)
     quit = tkinter.Button(main, text="Quit", command=main.quit)
     quit.grid(row=7, column=2, columnspan=2)
     tkinter.mainloop()
-
-
-
 
 
 if __name__ == '__main__':
