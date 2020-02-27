caa7fcac567b42a339d377d40cfa913d2c6a3a48
StringerJ <stringerj@allegheny.edu>
2020-02-27 15:36:56 -0500

    Fixed import for everything except pyautogui for RD Maze, and reformatted RD Maze

--- Recursive_Division/Modules/CheckValue.py
+++ Recursive_Division/Modules/CheckValue.py
@@ -1,22 +1,22 @@
 # Author: Jacob Stringer
 
+
 def cV(sNi, flNi, value):
     type = "none"
     if isinstance(value, str):
-        if sNi == True:
-            if ("0" or "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9") in value:
+        if sNi is True:
+            if ("0" or "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or
+               "9") in value:
                 type = "iStr"
             else:
                 type = "str"
-        if sNi == False:
+        if sNi is False:
             type = "str"
 
-
-
     if isinstance(value, int):
-        if flNi == True:
+        if flNi is True:
             type = "float"
-        elif flNi == False:
+        elif flNi is False:
             type = "int"
 
     if isinstance(value, float):
